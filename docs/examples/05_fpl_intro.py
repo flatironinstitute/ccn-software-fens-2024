9d7a8bfbf50a54bffae6c15b2aa39495c43238c6
Caitlin <caitlin9165@gmail.com>
2024-06-18 18:03:28 -0400

    small edits

--- docs/examples/05_fpl_intro.py
+++ docs/examples/05_fpl_intro.py
@@ -37,6 +37,7 @@
 import fastplotlib as fpl
 import numpy as np
 import imageio.v3 as iio
+
 # %%
 # ## Simple Image
 
@@ -100,6 +101,8 @@ image_graphic.reset_vmin_vmax()
 #
 # **Question:** Can you change the data of the image to be random data of the same shape? Hint: Use `np.random.rand()`
 # to create the new data.
+#
+# Note: At first you should see a solid image. You will also need to rest the vmin/vmax.
 
 # create new random data of the same shape as the original image
 new_data = np.random.rand(*image_graphic.data.value.shape)
@@ -131,14 +134,16 @@ fig.canvas.max_buffered_frames = 1
 data = np.random.rand(512, 512)
 
 # plot the data
-fig_v[0,0].add_image(data=data, name="random-image")
+fig_v[0, 0].add_image(data=data, name="random-image")
+
 
 # a function to update the image_graphic
-# a plot will pass its plot instance to the animation function as an arugment
+# a plot will pass its plot instance to the animation function as an argument
 
 def update_data(plot_instance):
     new_data = np.random.rand(512, 512)
-    plot_instance[0,0]["random-image"].data = new_data
+    plot_instance[0, 0]["random-image"].data = new_data
+
 
 #add this as an animation function
 fig_v.add_animations(update_data)
@@ -175,17 +180,19 @@ practice_fig = fpl.Figure()
 
 data = np.random.rand(512, 512)
 
-practice_fig[0,0].add_image(data=data, name="random-image", cmap="viridis")
+practice_fig[0, 0].add_image(data=data, name="random-image", cmap="viridis")
 
 # set the top-left and bottom-right quadrants of the data to 1
-practice_fig[0,0]["random-image"].data[:256, :256] = 1 # top-left
-practice_fig[0,0]["random-image"].data[256:, 256:] = 1 # bottom-right
+practice_fig[0, 0]["random-image"].data[:256, :256] = 1  # top-left
+practice_fig[0, 0]["random-image"].data[256:, 256:] = 1  # bottom-right
+
 
 # define an animation function to toggle the data
 def update_data(plot_instance):
     # set the top-right and bottom-left quadrants with new random data
-    plot_instance[0,0]["random-image"].data[:256, 256:] = np.random.rand(256, 256) # bottom-left
-    plot_instance[0,0]["random-image"].data[256:, :256] = np.random.rand(256, 256) # top-right
+    plot_instance[0, 0]["random-image"].data[:256, 256:] = np.random.rand(256, 256)  # bottom-left
+    plot_instance[0, 0]["random-image"].data[256:, :256] = np.random.rand(256, 256)  # top-right
+
 
 # add the animation function
 practice_fig.add_animations(update_data)
@@ -210,16 +217,16 @@ practice_fig.close()
 xs = np.linspace(-10, 10, 100)
 # sine wave
 ys = np.sin(xs)
-sine = np.dstack([xs, ys])[0]
+sine = np.column_stack([xs, ys])
 
 # cosine wave
-ys = np.cos(xs) + 5
-cosine = np.dstack([xs, ys])[0]
+ys = np.cos(xs)
+cosine = np.column_stack([xs, ys])
 
 # sinc function
 a = 0.5
-ys = np.sinc(xs) * 3 + 8
-sinc = np.dstack([xs, ys])[0]
+ys = np.sinc(xs) * 3
+sinc = np.column_stack([xs, ys])
 
 # %%
 # **We will plot all of it on the same plot. Each line plot will be an individual `Graphic`, you can have any
@@ -234,11 +241,11 @@ subplot = fig_lines[0, 0]
 sine_graphic = subplot.add_line(data=sine, thickness=5, colors="magenta")
 
 # you can also use colormaps for lines!
-cosine_graphic = subplot.add_line(data=cosine, thickness=12, cmap="autumn")
+cosine_graphic = subplot.add_line(data=cosine, thickness=12, cmap="autumn", offset=(0, 5, 0))
 
 # or a list of colors for each datapoint
 colors = ["r"] * 25 + ["purple"] * 25 + ["y"] * 25 + ["b"] * 25
-sinc_graphic = subplot.add_line(data=sinc, thickness=5, colors = colors)
+sinc_graphic = subplot.add_line(data=sinc, thickness=5, colors=colors, offset=(0, 8, 0))
 
 # show the plot
 fig_lines.show(sidecar=True, sidecar_kwargs={"title": "lines"})
@@ -255,7 +262,15 @@ cosine_graphic.colors[60] = "w"
 
 # indexing to assign colormaps to entire lines or segments
 sinc_graphic.cmap[10:50] = "gray"
-sine_graphic.cmap = "seismic"
+
+
+# %%
+
+# use a qualitative cmap (https://matplotlib.org/stable/_images/sphx_glr_colormaps_006.png)
+sine_graphic.cmap = "plasma"
+# set the cmap transform
+sine_graphic.cmap.transform = [4] * 25 + [1] * 25 + [3] * 25 + [8] * 25
+
 
 # %%
 #
@@ -264,9 +279,8 @@ sine_graphic.cmap = "seismic"
 fig_lines.close()
 
 # %%
-# #### 2D Lines Practice {.keep-code,.keep-text}
+# We are going to use the same sine and cosine data from before and do some practice!
 
-# same plot with sine and cosine wave only
 # Create a figure
 fig_lines = fpl.Figure()
 
@@ -277,12 +291,13 @@ subplot = fig_lines[0, 0]
 sine_graphic = subplot.add_line(data=sine, thickness=5, colors="magenta")
 
 # you can also use colormaps for lines!
-cosine_graphic = subplot.add_line(data=cosine, thickness=12, cmap="autumn")
+cosine_graphic = subplot.add_line(data=cosine, thickness=12, cmap="autumn", offset=(0, 5, 0))
 
 # show the plot
 fig_lines.show(sidecar=True, sidecar_kwargs={"title": "lines"})
 
-# %% {.strip-code, .keep-text}
+# %%
+# #### 2D Lines Practice {.strip-code,.keep-text}
 # **Question:** Can you change the colormap of the sine_graphic to "hsv"?
 
 sine_graphic.cmap = "hsv"
@@ -297,6 +312,7 @@ sinc_graphic.colors[:50] = "g"
 # last 50 data points of the sine_graphic?
 
 cosine_graphic.colors[50:] = sine_graphic.colors[50:]
+
 
 # %%
 # #### Capture changes to graphic properties as events
@@ -320,11 +336,13 @@ cosine_graphic.colors[50:] = sine_graphic.colors[50:]
 def callback_func(ev):
     print(ev.info)
 
+
 # %%
 
 # when the cosine graphic colors change, will also update the sine_graphic colors
 def change_colors(ev):
-   sine_graphic.colors[ev.info["key"]] = "w"
+    sine_graphic.colors[ev.info["key"]] = "w"
+
 
 cosine_graphic.add_event_handler(change_colors, "colors")
 
@@ -333,6 +351,7 @@ cosine_graphic.colors[:10] = "g"
 
 # %%
 fig_lines.close()
+
 
 # %%
 # ### More Events :D
@@ -346,48 +365,58 @@ def make_circle(center, radius: float, n_points: int = 75) -> np.ndarray:
 
     return np.column_stack([xs, ys]) + center
 
+
 # this makes 5 circles, so we can create 5 cmap values, so it will use these values to set the
 # color of the line based by using the cmap as a LUT with the corresponding cmap_value
 circles = list()
 for x in range(0, 50, 10):
     circles.append(make_circle(center=(x, 0), radius=4, n_points=100))
 
-# things like class labels, cluster labels, etc.
-# see the tab10 colormap [here](https://matplotlib.org/stable/_images/sphx_glr_colormaps_006.png)
-cmap_transform = [
-    0, 1, 2, 1, 0,
-]
 
 # %%
 
 fig = fpl.Figure()
 
-circles_graphic = fig[0,0].add_line_collection(data=circles, cmap="tab10", cmap_transform=cmap_transform, thickness=10)
+circles_graphic = fig[0, 0].add_line_collection(data=circles, cmap="tab10", thickness=10)
 
 # %%
 
 fig.show()
 
+
 # %%
 
 # get graphic that is clicked and change the color
 def click_event(ev):
+    # reset the colors
+    circles_graphic.cmap = "tab10"
+
+    # change clicked circle color
     ev.graphic.colors = "w"
+
 
 # %%
 
 # for each circle in the collection, add click event handler
 circles_graphic.add_event_handler(click_event, "click")
 
+
 # %%
 # #### Events Practice {.keep-text,.strip-code}
 #
 # **Question:** Can you add another event handler (using either method) to the circles_graphic that will change the
-# colormap of first 50 points in the circle when the colors of one of the circles changes?
+# thickness of a circle to 5 when you hover over it?
+#
+# Hint: the event type should be "pointer_move" :)
 
-@circles_graphic.add_event_handler("colors")
-def change_cmap(ev):
-    ev.graphic.cmap[:50] = "hsv"
+@circles_graphic.add_event_handler("pointer_move")
+def hover_event(ev):
+    # reset the thickness of all the circles
+    circles_graphic.thickness = [10] * len(circles_graphic)
+
+    # change the thickness of the circle that was hovered over
+    ev.graphic.thickness = 5
+
 
 # %%
 
@@ -411,6 +440,7 @@ fig[0, 0].auto_scale()
 
 fig.show(maintain_aspect=False)
 
+
 # %%
 
 # change the color of the sine wave based on the location of the linear selector
@@ -423,6 +453,7 @@ def set_color_at_index(ev):
     # change the color of the sine graphic at the index of the selector
     g.colors[ix] = "green"
 
+
 # %%
 
 # close the plot
@@ -434,7 +465,7 @@ fig.close()
 fig = fpl.Figure((2, 1))
 
 # data to plot
-xs = np.linspace(0, 10* np.pi, 1_000)
+xs = np.linspace(0, 10 * np.pi, 1_000)
 sine = np.sin(xs)
 sine += 100
 
@@ -450,6 +481,7 @@ zoomed_init = ls_x.get_selected_data()
 # make a line graphic for displaying zoomed data
 zoomed_x = fig[1, 0].add_line(zoomed_init)
 
+
 @ls_x.add_event_handler("selection")
 def set_zoom_x(ev):
     """sets zoomed x selector data"""
@@ -459,9 +491,10 @@ def set_zoom_x(ev):
     # remove the current zoomed data
     # and update with new selected data
     global zoomed_x
-    fig[1,0].remove_graphic(zoomed_x)
-    zoomed_x = fig[1,0].add_line(selected_data)
+    fig[1, 0].remove_graphic(zoomed_x)
+    zoomed_x = fig[1, 0].add_line(selected_data)
     fig[1, 0].auto_scale()
+
 
 fig.show(maintain_aspect=False)
 
@@ -472,10 +505,8 @@ fig.close()
 
 # %%
 # ## For more examples, please see our gallery:
+#
+# ### https://fastplotlib.readthedocs.io/en/latest/_gallery/index.html
 
 # %%
 # ## For a more comprehensive intro to the library, please see our guide:
-
-
-
-
